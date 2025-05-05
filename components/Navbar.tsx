0f2b428ec05b2f1220efe55b47af971614bb14f8
Benediktus Arthur Suparto <arthursuparto@gmail.com>
2025-05-05 07:44:29 +0700

    fixed history Page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,112 +14,106 @@ const items = [
     { label: "Dashboard", link: "/" },
     { label: "Transactions", link: "/transactions" },
     { label: "Manage", link: "/manage" },
-  ];
-  
-
+];
 
 function Navbar() {
   return (
-      <>
-  
-         <DesktopNavbar />
-         <MobileNavbar />
+    <>
+      <DesktopNavbar />
+      <MobileNavbar />
     </>
   )
 }
 
 function MobileNavbar() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <div className="block border-separate bg-background md:hidden">
-            <nav className="container mx-auto flex items-center justify-between px-8">
-                <Sheet open={isOpen} onOpenChange={setIsOpen}>
-                    <SheetTrigger asChild>
-                        <Button variant="ghost" size="icon">
-                            <Menu />
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent className="w-[85vw] max-w-sm" side="left">
-                        <Logo />
-                        <div className="flex flex-col gap-1 pt-4">
-                            {items.map((item) => (
-                                <NavbarItem
-                                    key={item.label}
-                                    link={item.link}
-                                    label={item.label}
-                                    onClick={() => setIsOpen((prev =>!prev))}
-                                />
-                            ))}
-                        </div>
-                    </SheetContent>
-                </Sheet>
-
-                <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
-                    <LogoMobile />
-                </div>
-                <div className="flex items-center gap-2">
-                    <ThemeSwitcherBtn />
-                    <UserButton afterSignOutUrl="/sign-in" />
-                </div>
-            </nav>
-        </div>
-    );
-}
-
-
-
-function DesktopNavbar() {
-    return (
-      <div className="container mx-auto justify-between hidden border-separate border-b bg-background md:block overflow-x-hidden">
-        {/* container mx-auto flex flex-wrap items-center justify-between gap-6 py-8 */}
-        <nav className="container flex items-center justify-between px-8">
-          <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+  return (
+    <div className="sticky top-0 z-50 border-b bg-background md:hidden"> {/* <-- UPDATED */}
+      <nav className="container mx-auto flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[85vw] max-w-sm" side="left">
             <Logo />
-            <div className="flex h-full">
+            <div className="flex flex-col gap-1 pt-4">
               {items.map((item) => (
                 <NavbarItem
                   key={item.label}
                   link={item.link}
                   label={item.label}
+                  onClick={() => setIsOpen((prev) => !prev)}
                 />
               ))}
             </div>
-          </div>
-        <div className="flex items-center gap-2">
-              <ThemeSwitcherBtn/>
-              <UserButton afterSignOutUrl="/sign-in"/>
+          </SheetContent>
+        </Sheet>
 
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
         </div>
-        </nav>
-      </div>
-    );
-  }
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
 
-  function NavbarItem({ link, label, onClick}: { link: string; label: string; onClick?: () => void }) {
-    const pathname = usePathname();
-    const isActive = pathname === link;
-  
-    return (
-      <div className="relative flex items-center justify-center"> 
-        <Link
-          href={link}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
-            isActive && "text-foreground"
-          )}
-          onClick={() => {
-            if (onClick) onClick();
-          }}
-        >
-          {label}
-        </Link>
-        {isActive && (
-          <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block" />
+function DesktopNavbar() {
+  return (
+    <div className="sticky  top-0 z-50 hidden border-b bg-background md:block"> 
+      <nav className="mx-4 flex items-center justify-between px-8">
+
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <Logo />
+          <div className="flex h-full">
+            {items.map((item) => (
+              <NavbarItem
+                key={item.label}
+                link={item.link}
+                label={item.label}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function NavbarItem({ link, label, onClick }: { link: string; label: string; onClick?: () => void }) {
+  const pathname = usePathname();
+  const isActive = pathname === link;
+
+  return (
+    <div className="relative flex items-center justify-center">
+      <Link
+        href={link}
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
+          isActive && "text-foreground"
         )}
-      </div>
-    );
-  }
-  
-export default Navbar
+        onClick={() => {
+          if (onClick) onClick();
+        }}
+      >
+        {label}
+      </Link>
+      {isActive && (
+        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block" />
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
